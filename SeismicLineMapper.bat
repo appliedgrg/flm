a000b67f78113e4d8b963ba3c0eaec6c4b78e1f4
Gus <gustavo.lopes.queiroz@gmail.com>
2020-02-06 10:14:17 -0700

    Updated bat file to work with multiple ArcMap installs

--- SeismicLineMapper.bat
+++ SeismicLineMapper.bat
@@ -5,27 +5,29 @@ set desktop=c:\python27\python.exe
 set default27=c:\python27\
 
 if exist "%pro%.bat" ( 
-    call "%pro%" %scriptName% 
-) else ( 
-	if exist %desktop% (
-        %desktop% %scriptName% %* 
-	) else (
-		if exist %default27% (
-			for /d %%i in (%default27%*) do (
-				if exist %%i\python.exe (
-					%%i\python.exe %scriptName% %*
-					goto :break 
-				) 
-			)
-		) else (
-		
-			python %scriptName% %* 
-			if errorlevel 1 (
-			    echo Python was not found.
-				pause
-			)
+	call "%pro%" %scriptName%
+	goto :break
+)
+
+if exist %default27% (
+	for /d %%i in (%default27%*) do (
+		if exist %%i\python.exe (
+			%%i\python.exe %scriptName% %*
+			goto :break
 		)
 	)
+)
+
+if exist %desktop% (
+	%desktop% %scriptName% %* 
+	goto :break
+) else (
+	python %scriptName% %* 
+)
+
+if errorlevel 1 (
+		echo Python was not found.
+		pause
 )
 
 :break
