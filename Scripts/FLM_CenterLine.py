84fb1ddfd518c08e9a4d3943867ea7deff846e77
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-03-18 13:04:38 -0600

    Refactor script to be used from automation scripts. Adding argument for main function and logs.

--- Scripts/FLM_CenterLine.py
+++ Scripts/FLM_CenterLine.py
@@ -25,34 +25,29 @@
 # Purpose: Determines the least cost path between vertices of the input lines
 #
 # ---------------------------------------------------------------------------
-
+import os
 import multiprocessing
 import arcpy
 from arcpy.sa import *
 arcpy.CheckOutExtension("Spatial")
-from . import FLM_Common as slmc
+import FLM_Common as slmc
 
-# Setup script path and workspace folder
 workspaceName = "FLM_CL_output"
-outWorkspace = slmc.GetWorkspace(workspaceName)
-arcpy.env.workspace = outWorkspace
-arcpy.env.overwriteOutput = True
-
-# Load arguments from file
-args = slmc.GetArgs("FLM_CL_params.txt")
-
-# Tool arguments
-Forest_Line_Feature_Class = args[0].rstrip()
-Cost_Raster = args[1].rstrip()
-Line_Processing_Radius = args[2].rstrip()
-ProcessSegments = args[3].rstrip()=="True"
-Output_Centerline = args[4].rstrip()
 
 def PathFile(path):
 	return path[path.rfind("\\")+1:]
-	
+
 def workLines(lineNo):
 	#Temporary files
+	outWorkspace = slmc.GetWorkspace(workspaceName)
+
+	# read params from text file
+	f = open(outWorkspace + "\\params.txt")
+	Forest_Line_Feature_Class = f.readline().strip()
+	Cost_Raster = f.readline().strip()
+	Line_Processing_Radius = f.readline().strip()
+	f.close()
+
 	fileSeg = outWorkspace +"\\FLM_CL_Segment_" + str(lineNo) +".shp"
 	fileOrigin = outWorkspace +"\\FLM_CL_Origin_" + str(lineNo) +".shp"
 	fileDestination = outWorkspace +"\\FLM_CL_Destination_" + str(lineNo) +".shp"
@@ -90,7 +85,7 @@ def workLines(lineNo):
 	xy = (float(x1),float(y1))
 	cursor.insertRow([xy])
 	del cursor
-	
+
 	# Create destination feature class
 	arcpy.CreateFeatureclass_management(outWorkspace,PathFile(fileDestination),"POINT",Forest_Line_Feature_Class,"DISABLED","DISABLED",Forest_Line_Feature_Class)
 	cursor = arcpy.da.InsertCursor(fileDestination, ["SHAPE@XY"])
@@ -106,14 +101,14 @@ def workLines(lineNo):
 		DescBuffer = arcpy.Describe(fileBuffer)
 		SearchBox = str(DescBuffer.extent.XMin)+" "+str(DescBuffer.extent.YMin)+" "+str(DescBuffer.extent.XMax)+" "+str(DescBuffer.extent.YMax)
 		arcpy.Clip_management(Cost_Raster, SearchBox, fileClip, fileBuffer, "", "ClippingGeometry", "NO_MAINTAIN_EXTENT")
-		
+
 		# Least cost path
 		arcpy.gp.CostDistance_sa(fileOrigin, fileClip, fileCostDist, "", fileCostBack, "", "", "", "", "TO_SOURCE")
 		arcpy.gp.CostPathAsPolyline_sa(fileDestination, fileCostDist, fileCostBack, fileCenterLine, "BEST_SINGLE", "")
-	
+
 	except:
 		print("Problem with line starting at X "+str(x1)+", Y "+str(y1)+"; and ending at X "+str(x1)+", Y "+str(y1)+".")
-	
+
 	#Clean temporary files
 	arcpy.Delete_management(fileSeg)
 	arcpy.Delete_management(fileOrigin)
@@ -123,33 +118,65 @@ def workLines(lineNo):
 	arcpy.Delete_management(fileCostDist)
 	arcpy.Delete_management(fileCostBack)
 
-def main():	
+def main(argv):
+	# Setup script path and workspace folder
+	global workspaceName
+	workspaceName = "FLM_CL_output"
+
 	global outWorkspace
 	outWorkspace = slmc.SetupWorkspace(workspaceName)
+	#outWorkspace = slmc.GetWorkspace(workspaceName)
+	arcpy.env.workspace = outWorkspace
+	arcpy.env.overwriteOutput = True
+
+	# Load arguments from file
+	if argv:
+		args = argv
+	else:
+		args = slmc.GetArgs("FLM_CL_params.txt")
+
+	# Tool arguments
+	global Forest_Line_Feature_Class
+	Forest_Line_Feature_Class = args[0].rstrip()
+	global Cost_Raster
+	Cost_Raster = args[1].rstrip()
+	global Line_Processing_Radius
+	Line_Processing_Radius = args[2].rstrip()
+	ProcessSegments = args[3].rstrip() == "True"
+	Output_Centerline = args[4].rstrip()
+
+	# write params to text file
+	f = open(outWorkspace + "\\params.txt", "w")
+	f.write(Forest_Line_Feature_Class + "\n")
+	f.write(Cost_Raster + "\n")
+	f.write(Line_Processing_Radius + "\n")
+	f.close()
 
 	#Prepare input lines for multiprocessing
 	numLines = slmc.SplitLines(Forest_Line_Feature_Class, outWorkspace, "CL", ProcessSegments)
-	
+
 	pool = multiprocessing.Pool(processes=slmc.GetCores())
 	slmc.log("Multiprocessing center lines...")
 	pool.map(workLines, range(1,numLines+1))
 	pool.close()
 	pool.join()
-	
+
 	slmc.logStep("Center line multiprocessing")
-	
+
 	slmc.log("Merging footprint layers...")
 	tempShapefiles = arcpy.ListFeatureClasses()
-	
-	arcpy.Merge_management(tempShapefiles,Output_Centerline)
+
+	if tempShapefiles:
+		arcpy.Merge_management(tempShapefiles,Output_Centerline)
 
 	slmc.logStep("Merging")
-		
+
 	for shp in tempShapefiles:
 		arcpy.Delete_management(shp)
-	
-	arcpy.AddField_management(Output_Centerline, "CorridorTh","DOUBLE")
-	arcpy.CalculateField_management(Output_Centerline, "CorridorTh","3")
-	
+
+	if os.path.exists(Output_Centerline):
+		arcpy.AddField_management(Output_Centerline, "CorridorTh","DOUBLE")
+		arcpy.CalculateField_management(Output_Centerline, "CorridorTh","3")
+
 if __name__ == '__main__':
 	main()
