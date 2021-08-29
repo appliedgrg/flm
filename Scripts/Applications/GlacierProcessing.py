6f8f3172d6dc74a13a6f001d33dc1130257497a6
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-08-29 17:25:12 -0600

    Script for process glacier data. It is also example for using FLM_Tools interface.

---
+++ Scripts/Applications/GlacierProcessing.py
@@ -0,0 +1,48 @@
+# System imports
+import os
+import sys
+
+# ArcGIS imports
+import arcpy
+
+# Add Scripts folder to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+# Local imports
+import FLM_Tools
+
+def main():
+
+    """ Prepare input seismic lines and raster for each cell"""
+
+    # Add Scripts folder to sys.path
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+    # Set arcpy environment variables
+    arcpy.env.overwriteOutput = True
+
+    # Coordinate Reference System: NAD 1983 10TM AEP Forest
+    arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(3714)
+
+    # Execute MakeNetCDFRasterLayer
+    # Check out the ArcGIS Spatial Analyst extension license
+    arcpy.CheckOutExtension("Spatial")
+
+    # traverse all the rows to prepare for extracting
+    spatialReference = arcpy.SpatialReference(3714)
+
+    # process footprint
+    baseDir = r"C:\Temp\Multi_Intersection_Streams_withSDM_NoFilledDEM"
+    in_line = os.path.join(baseDir, r"HC_MC_Multi_Intersection_Stream.shp")
+    in_line_footprint = os.path.join(baseDir, r"CenterlineOutput\FLMcenterline_output.shp")
+    inCanopyRaster = os.path.join(baseDir, r"Clip_CanopyRaster.tif")
+    in_cost_raster = os.path.join(baseDir, r"Clip_CostRaster.tif")
+    out_center_line = os.path.join(baseDir, r"CenterlineOutput\output_centerline.shp")
+    outFootprint = os.path.join(baseDir, r"Footprint\out_footprint.shp")
+
+    FLM_Tools.centerline(in_line, in_cost_raster, out_center_line)
+    #FLM_Tools.lineFootprint(inCenterline, inCanopyRaster, inCostRaster, outFootprint)
+
+
+if __name__ == "__main__":
+    main()
