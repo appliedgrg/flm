65ce99ff708e8e2993c1874bd3e2c4f9a72c9835
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-08-30 19:30:46 -0600

    General code re-formatting

--- Scripts/FLM_Attribute_Functions.py
+++ Scripts/FLM_Attribute_Functions.py
@@ -31,40 +31,44 @@
 import arcpy
 import FLM_Common as flmc
 
+
 def PathFile(path):
-	return path[path.rfind("\\")+1:]
+    return path[path.rfind("\\") + 1:]
+
 
 def FlmLineSplit(workspace, Input_Lines, SamplingType, Segment_Length, Tolerance_Radius):
+    if SamplingType == "IN-FEATURES":
+        return Input_Lines
 
-	if (SamplingType == "IN-FEATURES"):
-		return Input_Lines
-		
-	arcpy.env.workspace = workspace
-	arcpy.env.overwriteOutput = True
-	
-	FLA_Line_Unsplit = workspace+"\\FLA_Line_Unsplit.shp"
-	FLA_Line_Unsplit_Single = workspace+"\\FLA_Line_Unsplit_Single.shp"
-	FLA_Line_Split_Vertices = workspace+"\\FLA_Line_Split_Vertices.shp"
-	FLA_Segmented_Lines = workspace+"\\FLA_Segmented_Lines.shp"
-	
-	flmc.log("FlmLineSplit: Executing UnsplitLine")
-	flmc.log("Input_Lines: " + Input_Lines)
-	flmc.log("FLA_Line_Unsplit: " + FLA_Line_Unsplit)
-	
-	arcpy.UnsplitLine_management(Input_Lines,FLA_Line_Unsplit)
-	arcpy.MultipartToSinglepart_management(FLA_Line_Unsplit,FLA_Line_Unsplit_Single)
-	arcpy.Delete_management(FLA_Line_Unsplit)
+    arcpy.env.workspace = workspace
+    arcpy.env.overwriteOutput = True
 
-	if (SamplingType == "ARBITRARY"):
-		arcpy.GeneratePointsAlongLines_management(FLA_Line_Unsplit_Single, FLA_Line_Split_Vertices, "DISTANCE", Segment_Length, "", "NO_END_POINTS")
-	elif (SamplingType == "LINE-CROSSINGS"):
-		arcpy.Intersect_analysis( PathFile(FLA_Line_Unsplit_Single),  PathFile(FLA_Line_Split_Vertices), join_attributes="ALL", cluster_tolerance=Tolerance_Radius, output_type="POINT")
-		
-	if (SamplingType != "WHOLE-LINE"):
-		arcpy.SplitLineAtPoint_management(FLA_Line_Unsplit_Single, FLA_Line_Split_Vertices, FLA_Segmented_Lines, Tolerance_Radius)
-		arcpy.Delete_management(FLA_Line_Unsplit_Single)
-		arcpy.Delete_management(FLA_Line_Split_Vertices)
-	else:
-		FLA_Segmented_Lines = FLA_Line_Unsplit_Single
-	
-	return FLA_Segmented_Lines
+    FLA_Line_Unsplit = workspace + "\\FLA_Line_Unsplit.shp"
+    FLA_Line_Unsplit_Single = workspace + "\\FLA_Line_Unsplit_Single.shp"
+    FLA_Line_Split_Vertices = workspace + "\\FLA_Line_Split_Vertices.shp"
+    FLA_Segmented_Lines = workspace + "\\FLA_Segmented_Lines.shp"
+
+    flmc.log("FlmLineSplit: Executing UnsplitLine")
+    flmc.log("Input_Lines: " + Input_Lines)
+    flmc.log("FLA_Line_Unsplit: " + FLA_Line_Unsplit)
+
+    arcpy.UnsplitLine_management(Input_Lines, FLA_Line_Unsplit)
+    arcpy.MultipartToSinglepart_management(FLA_Line_Unsplit, FLA_Line_Unsplit_Single)
+    arcpy.Delete_management(FLA_Line_Unsplit)
+
+    if SamplingType == "ARBITRARY":
+        arcpy.GeneratePointsAlongLines_management(FLA_Line_Unsplit_Single, FLA_Line_Split_Vertices, "DISTANCE",
+                                                  Segment_Length, "", "NO_END_POINTS")
+    elif SamplingType == "LINE-CROSSINGS":
+        arcpy.Intersect_analysis(PathFile(FLA_Line_Unsplit_Single), PathFile(FLA_Line_Split_Vertices),
+                                 join_attributes="ALL", cluster_tolerance=Tolerance_Radius, output_type="POINT")
+
+    if SamplingType != "WHOLE-LINE":
+        arcpy.SplitLineAtPoint_management(FLA_Line_Unsplit_Single, FLA_Line_Split_Vertices, FLA_Segmented_Lines,
+                                          Tolerance_Radius)
+        arcpy.Delete_management(FLA_Line_Unsplit_Single)
+        arcpy.Delete_management(FLA_Line_Split_Vertices)
+    else:
+        FLA_Segmented_Lines = FLA_Line_Unsplit_Single
+
+    return FLA_Segmented_Lines
