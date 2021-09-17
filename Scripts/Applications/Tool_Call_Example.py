84c4f8bb7204e645daa12f5ad7a1a6d293b249ea
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-09-17 13:24:06 -0600

    Refactor example code.

--- Scripts/Applications/Tool_Call_Example.py
+++ Scripts/Applications/Tool_Call_Example.py
@@ -3,33 +3,16 @@ import os
 import sys
 
 # ArcGIS imports
-try:
-    import arcpy
-    arcpy.CheckOutExtension("Spatial")
-
-    if arcpy.CheckProduct("ArcInfo") == "Available":
-        msg = 'ArcGIS Advanced license is available'
-        print(msg)
-    else:
-        msg = 'ArcGIS Advanced license not available'
-        print(msg)
-        sys.exit(msg)
-
-except Exception as e:
-    print("Tool Call Example arcpy check.")
-    print(e)
-
-# Add Scripts folder to sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import arcpy
 
 # Local imports
+# Add Scripts folder to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import FLM_Tools
+
 
 def main():
     """ This is an example script for calling FLM tools for processing single dataset"""
-
-    # Add Scripts folder to sys.path
-    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
     # Set arcpy environment variables
     arcpy.env.overwriteOutput = True
@@ -41,14 +24,14 @@ def main():
     arcpy.CheckOutExtension("Spatial")
 
     # process footprint
-    baseDir = r"C:\FLM\Line_Editing_Staging\Region_3\Block_2\Cell_41"
-    in_chm = os.path.join(baseDir, r"Original\3_2_41_raster.tif")
-    in_line = os.path.join(baseDir, r"Edited\3_2_41_input_line_confirmed.shp")
-    in_canopy_raster = os.path.join(baseDir, r"Original\3_2_41_output_canopy.tif")
-    in_cost_raster = os.path.join(baseDir, r"Original\3_2_41_output_canopy.tif")
-    out_center_line = os.path.join(baseDir, r"Edited\3_2_41_output_center_line_confirmed-new.shp")
+    baseDir = r"C:\FLM\Line_Editing_Staging\Region_3\Block_2\Cell_42"
+    in_chm = os.path.join(baseDir, r"Original\3_2_42_raster.tif")
+    in_line = os.path.join(baseDir, r"Edited\3_2_42_input_line_confirmed.shp")
+    in_canopy_raster = os.path.join(baseDir, r"Original\3_2_42_output_canopy.tif")
+    in_cost_raster = os.path.join(baseDir, r"Original\3_2_42_output_cost.tif")
+    out_center_line = os.path.join(baseDir, r"Edited\3_2_42_output_center_line_confirmed-new.shp")
     in_line_ft = out_center_line
-    out_footprint = os.path.join(baseDir, r"Edited\3_2_41_footprint.shp")
+    out_footprint = os.path.join(baseDir, r"Edited\3_2_42_footprint.shp")
     out_attribute_whole = os.path.join(baseDir, r"Line_Attributes\temp_whole.shp")
 
     FLM_Tools.centerline(in_line, in_cost_raster, out_center_line)
