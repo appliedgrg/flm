c6e2ca5bffeee16b473d0d1b7f0fe68372c5c39a
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-09-21 19:15:48 -0600

    Update script to FLM_Tools

--- Scripts/Applications/ProcessCellsLineEditing.py
+++ Scripts/Applications/ProcessCellsLineEditing.py
@@ -9,12 +9,10 @@ import arcpy
 from arcpy.da import *
 from arcpy.sa import *
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
 # Local imports
-
+# Add Scripts folder to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import FLM_Tools
 
 # backup all output_center_line
 def BackupOutputCenterLine():
@@ -42,62 +40,9 @@ def RecoverOutputCenterLineShapefile(id):
         copyfile(src, dst)
 
 
-# Generate cost raster
-def CanopyCost():
-    import FLM_CanopyCost
-
-    print("Calculating number canopy cost: ", output_canopy_raster)
-    argv = [None]*8
-    argv[0] = input_raster           # CHM raster
-    argv[1] = paramCCHtThresh       # Canopy Height Threshold
-    argv[2] = paramCCSearchRadius    # Tree Search Radius
-    argv[3] = paramCCLineDist        # Maximum Line Distance
-    argv[4] = paramCCAvoidance       # Canopy Avoidance
-    argv[5] = paramCCExponent        # Cost Raster Exponent
-    argv[6] = output_canopy_raster   # Output Canopy Raster
-    argv[7] = output_cost_raster     # Output Cost Raster
-    print(argv[6])
-
-    if not os.path.exists(argv[6]) and not os.path.exists(argv[7]):
-        FLM_CanopyCost.main(argv)
-
-
-# Generate centerline
-def CenterLine():
-    import FLM_CenterLine
-
-    print("Calculating number center line: ", output_center_line)
-    argv = [None] * 5
-    argv[0] = input_line          # input line
-    argv[1] = output_cost_raster  # Cost raster
-    argv[2] = paramCLRadius       # line process radius
-    argv[3] = paramSegments       # Process segments
-    argv[4] = output_center_line  # Output center line
-
-    if not os.path.exists(argv[4]):
-        FLM_CenterLine.main(argv)
-
-
-# Generate line footprint
-def LineFootprint():
-    import FLM_LineFootprint
-
-    print("Calculating number canopy line footprint: ", output_line_footprint)
-    argv = [None] * 8
-    argv[0] = output_center_line     # center line
-    argv[1] = output_canopy_raster   # canopy raster
-    argv[2] = output_cost_raster     # Cost raster
-    argv[3] = "CorridorTh"           # corridor threshold field
-    argv[4] = paramLFLineWidth       # maximam line width
-    argv[5] = str(0)                 # expand and shrink cell range
-    argv[6] = paramSegments          # process segments
-    argv[7] = output_line_footprint  # Output line foot print
-
-    if os.path.exists(output_center_line) and not os.path.exists(argv[7]):
-        FLM_LineFootprint.main(argv)
-
-# Set layer style for aprx project
 def setLayerStyle(layer, lineColor, lineWidth, fillColor=None):
+    """Set layer style for aprx project"""
+
     cim_lyr = layer.getDefinition('V2')
 
     # Modify the color, width and dash template for the SolidStroke layer
@@ -115,10 +60,12 @@ def setLayerStyle(layer, lineColor, lineWidth, fillColor=None):
     # Push the changes back to the layer object
     layer.setDefinition(cim_lyr)
 
-# Prepare aprx project for Potoplot quality check
-# An group layer will be created for each photo plot
-# CHM, flm produced layers: center line, line footprint and rasters will be added to the group
+
 def PrepareArcGISproProject(cellPath, fileName):
+    """Prepare aprx project for Potoplot quality check
+       An group layer will be created for each photo plot
+       CHM, flm produced layers: center line, line footprint and rasters will be added to the group"""
+
     from distutils.dir_util import copy_tree
     copy_tree(r'Y:\Cell_Project', os.path.join(cellPath, r'Cell_Project'))
 
@@ -180,10 +127,14 @@ def ProcessCell(cellFolder, fileName, paramsList, lineType):
     cellPathOriginal = os.path.join(basePath, cellFolder, "Original")
     cellPathEdited = os.path.join(basePath, cellFolder, "Edited")
 
-    global input_raster
     input_raster = os.path.join(cellPathOriginal, fileName+"_raster.tif")
-    global input_line
     input_line = os.path.join(cellPathEdited, fileName + "_input_line_confirmed.shp")
+
+    # Verify if the input line exist
+    if not arcpy.Exists(input_raster) or not arcpy.Exists(input_line):
+        print("Input file(s) not exist.")
+        return
+
     seismic = {"conventional": "CONVENTIONAL-SEISMIC", "low_impact": "LOWIMPACT-SEISMIC", "trail": "TRAIL"}
     if lineType != "default":
         input_line_temp = fileName + "_input_line_" + lineType + ".shp"
@@ -195,23 +146,24 @@ def ProcessCell(cellFolder, fileName, paramsList, lineType):
     if lineType != "default":
         extension = "_" + lineType
 
-    global output_canopy_raster
     output_canopy_raster = os.path.join(cellPathOriginal, fileName+"_output_canopy"+extension+".tif")
-    global output_cost_raster
     output_cost_raster = os.path.join(cellPathOriginal, fileName+"_output_cost"+extension+".tif")
-    global output_center_line
-    output_center_line = os.path.join(cellPathEdited, fileName+"_output_center_line"+extension+".shp")
-    global output_line_footprint
+    output_center_line = os.path.join(cellPathEdited, fileName+"_output_centerline_confirmed"+extension+".shp")
     output_line_footprint = os.path.join(cellPathEdited, fileName+"_output_footprint"+extension+".shp")
 
+    # Verify if the input line exist
+    if not arcpy.Exists(output_canopy_raster) or not arcpy.Exists(output_cost_raster):
+        print("Input raster file(s) not exist.")
+        return
+
     # Generate cost raster
-    print("Cell {} canopy cost tool started.".format(fileName))
+    #print("Cell {} canopy cost tool started.".format(fileName))
     #CanopyCost()
-    print("Cell {} canopy cost tool finished.".format(fileName))
+    #print("Cell {} canopy cost tool finished.".format(fileName))
 
     # Generate center line
     print("Cell {} centerline tool started.".format(fileName))
-    CenterLine()
+    FLM_Tools.centerline(input_line, output_cost_raster, output_center_line)
     print("Cell {} centerline tool finished.".format(fileName))
 
     return output_center_line
@@ -241,7 +193,7 @@ def ProcessCells(cells, paramsList, basePath, discriminateLineType=False):
                 fileTemp = ProcessCell(cellPath, fileName, paramsList, paramsList[i])
                 outputLineFiles.append(fileTemp)
                 # Merge three types of output line
-                outputCenterLine = os.path.join(cellPath, fileName + "_output_center_line.shp")
+                outputCenterLine = os.path.join(cellPath, fileName + "_output_centerline.shp")
                 arcpy.Merge_management(outputLineFiles, outputCenterLine)
         else:
             ProcessCell(cellPath, fileName, paramsList, "default")
@@ -254,10 +206,6 @@ def ProcessCells(cells, paramsList, basePath, discriminateLineType=False):
 def main():
 
     """ Prepare input seismic lines and raster for each cell"""
-
-    # Add Scripts folder to sys.path
-    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
     # Set arcpy environment variables
     arcpy.env.overwriteOutput = True
 
@@ -268,19 +216,16 @@ def main():
     # Check out the ArcGIS Spatial Analyst extension license
     arcpy.CheckOutExtension("Spatial")
 
-    # traverse all the rows to prepare for extracting
-    spatialReference = arcpy.SpatialReference(3400)
-
     # Line Footprint Checkups:
     global basePath
-    basePath = r'E:\Line_Editing_Staging'
+    basePath = r'F:\Line_Editing_Staging'
 
-    #cells = [(x, y, z) for x in range(3,4) for y in range(2,3) for z in range(30, 31)]
-    cells = [(3, 2, 32)]
+    cells = [(x, y, z) for x in range(3, 4) for y in range(2, 3) for z in range(13, 61)]
+    # cells = [(3, 2, 41)]
     paramsList = {"conventional": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r""),
-              "low_impact": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r""),
-              "trail": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r""),
-              "default": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r"")}
+                  "low_impact": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r""),
+                  "trail": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r""),
+                  "default": ("2", "3", "10", "0.3", "1.5", "30", "32", "True", r"")}
 
     discriminateLineType = False
     ProcessCells(cells, paramsList, basePath, discriminateLineType)
