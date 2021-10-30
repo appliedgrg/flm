8b9721df8772e43da3035efe2de16b7741335b64
Richard Zeng <502763+RichardQZeng@users.noreply.github.com>
2021-10-29 23:14:48 -0600

    Finishing appendToGroup, groupIntersection, generateAnchorPairs.

--- Scripts/FLM_VertexOptimization.py
+++ Scripts/FLM_VertexOptimization.py
@@ -28,6 +28,9 @@
 # System imports
 import os
 import multiprocessing
+import numpy as np
+import math
+import uuid
 
 # ArcGIS imports
 import arcpy
@@ -38,7 +41,8 @@ arcpy.CheckOutExtension("Spatial")
 import FLM_Common as flmc
 
 workspaceName = "FLM_VO_output"
-global DISTANCE_THRESHOLD = 1  # 1 meter for intersection neighbourhood
+DISTANCE_THRESHOLD = 1  # 1 meter for intersection neighbourhood
+SEGMENT_LENGTH = 20  # Distance (meter) from intersection to anchor points
 
 
 def PathFile(path):
@@ -49,59 +53,148 @@ def appendToGroup(vertex, vertex_grp):
     Append new vertex to vertex group, by calculating distance to existing vertices
     """
     pt_added = False
+    global DISTANCE_THRESHOLD
+    global SEGMENT_LENGTH
 
-    for pt in vertex_grp:
-        if abs() < DISTANCE_THRESHOLD and  abs() < DISTANCE_THRESHOLD:
-            pt["lines"].append(vertex["lines"][0])
+    # Calculate anchor point for each vertex
+    point = arcpy.Point(vertex["point"][0], vertex["point"][1])
+    line = vertex["lines"][0][0]
+    index = vertex["lines"][0][1]
+    pts = ptsInLine(line)
+
+    if index == 0:
+        pt_1 = point
+        pt_2 = pts[1]
+    elif index == -1:
+        pt_1 = point
+        pt_2 = pts[-2]
+
+    dist_pt = math.sqrt(sum((px - qx) ** 2.0 for px, qx in zip([pt_1.X, pt_1.Y], [pt_2.X, pt_2.Y])))
+    X = pt_1.X + (pt_2.X - pt_1.X) * SEGMENT_LENGTH / dist_pt
+    Y = pt_1.Y + (pt_2.Y - pt_1.Y) * SEGMENT_LENGTH / dist_pt
+    vertex["lines"][0].append([X, Y])  # add anchor point to list (the third element)
+
+    for item in vertex_grp:
+        if abs(point.X-item["point"][0]) < DISTANCE_THRESHOLD and abs(point.Y-item["point"][1]) < DISTANCE_THRESHOLD:
+            item["lines"].append(vertex["lines"][0])
             pt_added = True
 
     # Add the first vertex or new vertex not found neighbour
     if not pt_added:
         vertex_grp.append(vertex)
 
+def ptsInLine(line):
+    point_list = []
+    for part in line:
+        for point in part:  # loops through every point in a line
+            # loops through every vertex of every segment
+            if point:  # adds all the vertices to segment_list, which creates an array
+                point_list.append(point)
+
+    return point_list
 
 def groupIntersections(lines):
     """
     Identify intersections of 2,3 or 4 lines and group them.
     Each group has all the end vertices, start(0) or end (-1) vertex and the line geometry
-    Intersection list format: [[intersection_pt, [[line_geom, pt_index], ...]], ...]
+    Intersection list format: {["point":intersection_pt, "lines":[[line_geom, pt_index, anchor_geom], ...]], ...}
     pt_index: 0 is start vertex, -1 is end vertex
     """
     vertex_grp = []
 
     for line in lines:
-        line_seg = line[0]
-
-        for line in line_seg:
-            for point in line:  # loops through every point in a line
-                # loops through every vertex of every segment
-                if point:  # adds all the vertices to segment_list, which creates an array
-                    segment_list.append(point)
+        point_list = ptsInLine(line[0])
 
         # Find origin and destination coordinates
-        pt_start = {"point":arcpy.Point(segment_list[0].X, segment_list[0].Y), "lines":[[line_seg, 0]]}
-        pt_end = {"point":arcpy.Point(segment_list[-1].X, segment_list[-1].Y), "lines":[[line_seg, -1]]}
+        pt_start = {"point": [point_list[0].X, point_list[0].Y], "lines": [[line[0], 0]]}
+        pt_end = {"point": [point_list[-1].X, point_list[-1].Y], "lines": [[line[0], -1]]}
         appendToGroup(pt_start, vertex_grp)
         appendToGroup(pt_end, vertex_grp)
 
     return vertex_grp
 
 
-def generateAnchorPtPairs():
+def getSlope(line, end_index):
     """
-    Generate buffer of the intersection and extend lines to make them intersect with the buffer polygon.
-    lines will be grouped along directions.
+    Calculate the slope of the first or last segment
+    line: ArcPy Polyline
+    end_index: 0 or -1 of the the line vertices. Consider the multipart.
     """
-    pass
+    pt = ptsInLine(line)
 
+    if end_index == 0:
+        pt_1 = pt[0]
+        pt_2 = pt[1]
+    elif end_index == -1:
+        pt_1 = pt[-1]
+        pt_2 = pt[-2]
 
-def leastCostPath(Cost_Raster, segment_info, Line_Processing_Radius):
+    return (pt_1.Y - pt_2.Y) / (pt_1.X - pt_2.X)
+
+def generateAnchorPairs(vertex):
+    """
+    Extend line following outward direction to length of SEGMENT_LENGTH
+    Use the end point as anchor point.
+        vertex: input intersection with all related lines
+        return: one or two pairs of anchors according to numbers of lines intersected.
+                two pairs anchors return when 3 or 4 lines intersected
+                one pair anchors return when 1 or 2 lines intersected
+    """
+    lines = vertex["lines"]
+    slopes = []
+    for line in lines:
+        line_seg = line[0]
+        pt_index = line[1]
+        slopes.append(getSlope(line_seg, pt_index))
+
+    index = 0 # the index of line which paired with first line.
+    if len(slopes) == 4 or len(slopes) == 3:
+        diff = [abs(slopes[0] - i) for i in slopes[1:-1]]  # calculate difference of first slopes with the rest
+        index = np.argmin(diff)
+
+        # first anchor pair
+        pt_start_1 = lines[0][2]
+        pt_end_1 = lines[index][2]
+
+        # the rest one or two index
+        a = {0, 1, 2, 3}
+        b = set([0, index])
+        remains = list(a.difference(b))  # the remaining index
+
+        if len(remains) == 2:
+            pt_start_2 = lines[index][2]
+            pt_end_2 = lines[index][2]
+        elif len(remains) == 1:
+            pt_start_2 = lines[index][2]
+            # symmetry point of pt_start_2 regarding vertex["point"]
+            X = vertex["point"].X - (pt_start_2.X - vertex["point"].X)
+            Y = vertex["point"].Y - (pt_start_2.Y - vertex["point"].Y)
+            pt_end_2 = arcpy.Point(X, Y)
+
+    # this scenario only use two anchors and find closest point on least cost path
+    if len(slopes) == 2:
+        pt_start_1 = lines[0][2]
+        pt_end_1 = lines[1][2]
+    elif len(slopes) == 1:
+        pt_start_1 = lines[0][2]
+        # symmetry point of pt_start_1 regarding vertex["point"]
+        X = vertex["point"].X - (pt_start_1.X - vertex["point"].X)
+        Y = vertex["point"].Y - (pt_start_1.Y - vertex["point"].Y)
+        pt_end_1 = arcpy.Point(X, Y)
+
+    if len(slopes) == 4 or len(slopes) == 3:
+        return [pt_start_1, pt_end_1, pt_start_2, pt_end_2]
+    elif len(slopes) == 2 or len(slopes) == 1:
+        return [pt_start_1, pt_end_1]
+
+def leastCostPath(Cost_Raster, anchors, Line_Processing_Radius):
     """
     Calculate least cost path between two points
-        pt_start: start point
-        pt_end: end point
+        Cost_Raster: cost raster
+        anchors: list of two points: start and end points
+        Line_Processing_Radius
     """
-    lineNo = segment_info[1]  # second element is the line No.
+    lineNo = uuid.uuid4().hex  # random line No.
     outWorkspaceMem = r"memory"
     arcpy.env.workspace = r"memory"
 
@@ -110,24 +203,12 @@ def leastCostPath(Cost_Raster, segment_info, Line_Processing_Radius):
     fileCostBack = os.path.join(outWorkspaceMem, "FLM_VO_CostBack_" + str(lineNo) + ".tif")
     fileCenterline = os.path.join(outWorkspaceMem, "FLM_VO_Centerline_" + str(lineNo))
 
-    # Load segment list
-    segment_list = []
-
-    for line in segment_info[0]:
-        for point in line:  # loops through every point in a line
-            # loops through every vertex of every segment
-            if point:  # adds all the vertices to segment_list, which creates an array
-                segment_list.append(point)
-
-    # Find origin and destination coordinates
-    x1 = segment_list[0].X
-    y1 = segment_list[0].Y
-    x2 = segment_list[-1].X
-    y2 = segment_list[-1].Y
-
+    # line from points
+    # TODO change the way to set spatial reference
+    line = arcpy.Polyline([arcpy.Point(*pt) for pt in anchors], arcpy.SpatialReference(3400))
     try:
         # Buffer around line
-        lineBuffer = arcpy.Buffer_analysis([segment_info[0]], arcpy.Geometry(), Line_Processing_Radius, "FULL", "ROUND",
+        lineBuffer = arcpy.Buffer_analysis([line], arcpy.Geometry(), Line_Processing_Radius, "FULL", "ROUND",
                                            "NONE", "", "PLANAR")
 
         # Clip cost raster using buffer
@@ -162,15 +243,11 @@ def leastCostPath(Cost_Raster, segment_info, Line_Processing_Radius):
     return centerline
 
 
-def workLinesMem(segment_info):
+def workLinesMem(vertex):
     """
     New version of worklines. It uses memory workspace instead of shapefiles.
     The refactoring is to accelerate the processing speed.
     """
-
-    # input verification
-    if segment_info is None or len(segment_info) <= 1:
-        print("Input segment is corrupted, ignore")
 
     # Temporary files
     outWorkspace = flmc.GetWorkspace(workspaceName)
@@ -182,10 +259,15 @@ def workLinesMem(segment_info):
     Line_Processing_Radius = float(f.readline().strip())
     f.close()
 
-    centerline = leastCostPath(Cost_Raster, segment_info, Line_Processing_Radius)
+    anchors = generateAnchorPairs(vertex)
+    if len(anchors) == 4:
+        centerline_1 = leastCostPath(Cost_Raster, anchors[0:2], Line_Processing_Radius)
+        centerline_2 = leastCostPath(Cost_Raster, anchors[2:4], Line_Processing_Radius)
+    elif len(anchors) == 2:
+        centerline_1 = leastCostPath(Cost_Raster, anchors, Line_Processing_Radius)
 
     # Return centerline
-    print("Processing line {} done".format(segment_info[1]))
+    print("Processing vertex {} done".format(vertex["point"]))
     return centerline
 
 
